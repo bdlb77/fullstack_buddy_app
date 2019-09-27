ffd7d9a00cc34c6736e3f0f072062c937c6e13e8
Bryan Leighton <bryan.leigh77@gmail.com>
2019-09-27 16:47:46 +0300

    refactor step model test

--- fullstack_buddy_backend/spec/models/step_spec.rb
+++ fullstack_buddy_backend/spec/models/step_spec.rb
@@ -7,10 +7,6 @@ RSpec.describe Step, type: :model do
   describe 'validations' do
     it {should validate_presence_of :sequential_number}
   end
-  describe 'has_many associations' do
-
-  end
-
   describe 'belongs_to associations' do
     it {should belong_to :project}
     it {should belong_to :feature}
